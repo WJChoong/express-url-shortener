404fec53eb9ec66b61ee70e1008790cc28bb3851
davidtan <davidtan@thoughtworks.com>
2018-03-16 14:10:41 +0800

    Revert "Experiment: change to port 3001"
    This reverts commit 7cd27d571dcda82e2328fc1e1cb8ddfdf706bd1d.

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const app = require("./app");
 
-const server = app.listen(3001, function() {
-  console.log(process.env.PORT);
+const server = app.listen(process.env.PORT || 3001, function() {
   console.log(`Listening on port ${server.address().port}...`);
 });
